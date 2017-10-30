3f4a377dd82de885c65e5f8b69922f310f77d4cd
IrinaRymar <ira.shaim@gmail.com>
2017-10-30 09:51:58 +0400

    Third Commit

---
+++ Second Test- 2 урок. Задание №3/java-example-litecart/src/test/java/ru/stqa/training/selenium/MySecondTest.java
@@ -0,0 +1,45 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MySecondTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,120);
+    }
+
+    @Test
+    public void mySecondTest(){
+        driver.get("http://localhost:10000/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(titleIs("My Store"));
+    }
+
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+}
